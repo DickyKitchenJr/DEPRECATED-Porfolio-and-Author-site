efbe72f6a7635de087fe3e1d88c5addd8663636b
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-04-22 13:48:39 -0400

    added to and improved JSPlayground
    - added name generator to JSPlayground
    - improved naming structures in HTML/CSS/JS and made small changes to CSS

--- JSPlayground/JSPlayground.js
+++ JSPlayground/JSPlayground.js
@@ -1,4 +1,6 @@
-//HSL change section
+//HSL Gradient change section
+
+//create random HSL
 function newHSL(){
     let hue = Math.floor(Math.random() * 361)
     let saturation = Math.floor(Math.random() * 101)
@@ -6,7 +8,7 @@ function newHSL(){
     let HSL = 'hsl('+ hue + ', ' + saturation + '%, ' + lightness + '%)'
     return HSL;
 }
-
+//assign 3 random HSL to gradient
 function newLinearGradient(){
     let newGradient = [['0'],['1'],['2']];
     let newLG = ''
@@ -19,20 +21,22 @@ function newLinearGradient(){
     document.getElementById('color2').innerHTML = newGradient[1];
     document.getElementById('color3').innerHTML = newGradient[2];
 };
-
+//reset all 3 HSL to white to reset background
 function plainHSL(){
     document.body.style.background = 'white'
     document.getElementById('color1').innerHTML = 'hsl(0, 0%, 100%)';
     document.getElementById('color2').innerHTML = 'hsl(0, 0%, 100%)';
     document.getElementById('color3').innerHTML = 'hsl(0, 0%, 100%)';
 }
-
-let clicker = document.getElementById('clickmeHSL');
-clicker.onclick = newLinearGradient;
+//generate random HSL gradient and reset HSL on respective button clicks
+let clickHSL = document.getElementById('clickmeHSL');
+clickHSL.onclick = newLinearGradient;
 let resetHSL = document.getElementById('resetHSL');
 resetHSL.onclick = plainHSL;
 
 //Dad joke section
+
+//array of jokes
 let jokes = ['Did you hear the one about the guy who fell through the eyeglass factory? Yeah... He made a real spectacle of himself.',
 'How many tickles does it take to make an octopus laugh? Ten... ten tickles', 
 'Which bear is the most condescending? A pan-duh!',
@@ -99,12 +103,62 @@ let jokes = ['Did you hear the one about the guy who fell through the eyeglass f
 'I’m not a fan of elevator music. I’s bad on so many levels.',
 'Why did the banana go to the doctor? He was peeling bad.',
 'What did the shy pebble wish for? To be a little boulder.',
-'Why do cows wear bells? Because their horns don’t work.']
+'Why do cows wear bells? Because their horns don’t work.'];
 
+//choose a joke to display at random
 function tellAJoke() {
     let ranNum = Math.floor(Math.random() * (jokes.length - 1));
     document.getElementById('joke').innerHTML = jokes[ranNum];
 }
+//generate random joke on button click
+let clickJoke = document.getElementById('jokebutton')
+clickJoke.onclick = tellAJoke;
 
-let jokeTime = document.getElementById('jokebutton')
-jokeTime.onclick = tellAJoke;
+//Non-sense Name Generator
+
+function nonSenseName() {
+    let consonant = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'];
+    let vowel = ['a', 'e', 'i', 'o', 'u', 'y'];
+    let digraph = ['wh', 'th', 'sh', 'ph', 'nt', 'ch', 'ck', 'ng', 'wr', 'br', 'gr', 'tr', 'cr', 'kr', 'kn', 'mb', 'gh', 'st', 'sl', 'kl', 'cl', 'bl', 'lc', 'lk'];
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let word = [];
+    let count = 0;
+    let ranNum = Math.floor(Math.random()*6) +2;
+    // using ranNum to generate a number between 2 and 8 to assign word random letters
+    while(count < ranNum){
+        word.push(letters[Math.floor(Math.random() * letters.length)]);
+        count ++;
+    };
+    console.log(word)
+    //check if consonants put together create digraph, if not change to vowel
+    for (const i in word) {
+      let each = Number(i);
+      let each1 = each + 1;
+      let checkDigraph = word[each] + word[each1];
+      if (consonant.includes(word[each]) && vowel.includes(word[each1]) == false && digraph.includes(checkDigraph) == false && each < word.length -1){
+        vowelTotalIndex = vowel.length -1;
+        word[each] =  vowel[Math.floor(Math.random() * vowelTotalIndex)];
+      }; 
+    }
+    console.log(word);
+    //check if more than 2 vowels are together, if so change middle vowel to digraph
+    for(const i in word){
+      let each = Number(i);
+      let beforeEach = each -1;
+      let afterEach = each + 1;
+      if(vowel.includes(word[each]) && vowel.includes(word[beforeEach]) && vowel.includes(word[afterEach])){
+        digraphTotalIndex = digraph.length -1;
+        word[each] = digraph[Math.floor(Math.random() * digraphTotalIndex)];
+      };
+    }
+    console.log(word);
+    // capitalize first letter then join array into a string
+    let firstLetter = word[0];
+    word[0] = firstLetter.toUpperCase();
+    word = word.join('');
+    //assign new word to webpage
+    document.getElementById('nameme').innerHTML = word;
+}
+//generate random name on button click
+let makeName = document.getElementById('namebutton')
+makeName.onclick = nonSenseName;
