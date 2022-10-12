e2a1756ea912d5cb8466b86021b889b1ae24c04e
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-10-12 11:54:52 -0400

    added modals

---
+++ DKCodes/dkcodes.js
@@ -0,0 +1,95 @@
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector(".modal-open");
+const closeModal = document.querySelector(".modal-close");
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+});
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+});
+
+const modal2 = document.querySelector(".modal2");
+const openModal2 = document.querySelector(".modal-open2");
+const closeModal2 = document.querySelector(".modal-close2");
+
+openModal2.addEventListener("click", () => {
+  modal2.showModal();
+});
+
+closeModal2.addEventListener("click", () => {
+  modal2.close();
+});
+
+const modal3 = document.querySelector(".modal3");
+const openModal3 = document.querySelector(".modal-open3");
+const closeModal3 = document.querySelector(".modal-close3");
+
+openModal3.addEventListener("click", () => {
+  modal3.showModal();
+});
+
+closeModal3.addEventListener("click", () => {
+  modal3.close();
+});
+
+const modal4 = document.querySelector(".modal4");
+const openModal4 = document.querySelector(".modal-open4");
+const closeModal4 = document.querySelector(".modal-close4");
+
+openModal4.addEventListener("click", () => {
+  modal4.showModal();
+});
+
+closeModal4.addEventListener("click", () => {
+  modal4.close();
+});
+
+const modal5 = document.querySelector(".modal5");
+const openModal5 = document.querySelector(".modal-open5");
+const closeModal5 = document.querySelector(".modal-close5");
+
+openModal5.addEventListener("click", () => {
+  modal5.showModal();
+});
+
+closeModal5.addEventListener("click", () => {
+  modal5.close();
+});
+
+const modal6 = document.querySelector(".modal6");
+const openModal6 = document.querySelector(".modal-open6");
+const closeModal6 = document.querySelector(".modal-close6");
+
+openModal6.addEventListener("click", () => {
+  modal6.showModal();
+});
+
+closeModal6.addEventListener("click", () => {
+  modal6.close();
+});
+
+const modal7 = document.querySelector(".modal7");
+const openModal7 = document.querySelector(".modal-open7");
+const closeModal7 = document.querySelector(".modal-close7");
+
+openModal7.addEventListener("click", () => {
+  modal7.showModal();
+});
+
+closeModal7.addEventListener("click", () => {
+  modal7.close();
+});
+
+const modal8 = document.querySelector(".modal8");
+const openModal8 = document.querySelector(".modal-open8");
+const closeModal8 = document.querySelector(".modal-close8");
+
+openModal8.addEventListener("click", () => {
+  modal8.showModal();
+});
+
+closeModal8.addEventListener("click", () => {
+  modal8.close();
+});
